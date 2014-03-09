770b856a74efff3a388619af64dec514836b3a48
Monarch Jhaveri <monarchjhaveri@gmail.com>
2014-03-08 19:38:41 -0500

    Still working on Problem 2

--- Problem2/linegraph.js
+++ Problem2/linegraph.js
@@ -16,9 +16,9 @@
 
     bbVis = {
         x: 0 + 100,
-        y: 10,
+        y: -height,
         w: width - 100,
-        h: 100
+        h: height
     };
 
     dataSet = [];
@@ -41,25 +41,72 @@
 
     createVis = function() {
         var xAxis, xScale, yAxis,  yScale;
+        var allPopulationPoints = [];
+        var allYears = [];
 
-          xScale = d3.scale.linear().domain([0,100]).range([0, bbVis.w]);   // define the right domain generically. TODO: get time
-          yScale = d3.scale.linear().domain([0,100]).range(0, bbVis.h]);    // define the 
+        // determine maximum and minimum populations
 
-		  // example that translates to the bottom left of our vis space:
-		  var visFrame = svg.append("g").attr({
-		      "transform": "translate(" + bbVis.x + "," + (bbVis.y + bbVis.h) + ")",
-		  	  //....
-			  
-		  });
-		  
-		  visFrame.append("rect");
+        dataSet.forEach(function(d){allPopulationPoints.push(d.HYDE, d.Maddison, d.PopulationBureau, d.UN, d.USCensus)});
+        var maximumPopulation = d3.max(allPopulationPoints, function(d){ if(d!="") return +d });
+        var minimumPopulation = d3.min(allPopulationPoints, function(d){ if(d!="") return +d });
+
+        // determine maximum and minimum dates
+
+        dataSet.forEach(function(d){ allYears.push(d.year)});
+        var maximumYear = new Date(Date.UTC(0,0,1)).setUTCFullYear( d3.max(allYears, function(d){if(d!="") return +d}));
+        var minimumYear = new Date(Date.UTC(0,0,1)).setUTCFullYear( d3.min(allYears, function(d){if(d!="") return +d}));
+
+        // create scales with minimum and maximum values
+
+        xScale = d3.time.scale()
+            .domain([minimumYear, maximumYear])
+            .range([0, bbVis.w]);
+        yScale = d3.scale.linear()
+            .range([0, bbVis.h])
+            .domain([minimumPopulation, maximumPopulation]);
+
+        // create Axes with scales
+        yAxis = d3.svg.axis()
+            .scale(yScale)
+            .orient("left");
+        xAxis = d3.svg.axis()
+            .scale(xScale)
+            .orient("bottom");
+
+
+		// example that translates to the bottom left of our vis space:
+		var visFrame = svg.append("g").attr({
+		    "transform": "translate(" + bbVis.x + "," + (bbVis.y + bbVis.h) + ")",
+            "class": "estimates",
+
+		  //....	  
+		});
+		  // visFrame.append("rect");
 		  //....
 		  
-//        yScale = .. // define the right y domain and range -- use bbVis
+        //attach the x  
+        visFrame.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0, "+bbVis.h+")")
+            .call(xAxis)
+        .append("text")
+            .attr("y", 25)
+            .attr("x", width/2)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Year");
 
-//        xAxis = ..
-//        yAxis = ..
-//        // add y axis to svg !
+        visFrame.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+        .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Population");
+
+
 
 
     };
