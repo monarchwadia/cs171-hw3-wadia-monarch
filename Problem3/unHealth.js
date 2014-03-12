26f521dc8964447cfcfdf8214356f9d5294e7f83
Monarch Jhaveri <monarchjhaveri@gmail.com>
2014-03-11 23:08:06 -0400

    separation

--- Problem3/unHealth.js
+++ Problem3/unHealth.js
@@ -7,7 +7,8 @@ convertToInt = function(s) {
 var bbDetail, bbOverview, dataSet, detaSetNoHeader, svg, detailGraph, overviewGraph, dataSetDates, dataSetWomensHealth;
 var yScaleDetail,
     yScaleOverview,
-    xScale,
+    xScaleOverview,
+    xScaleDetail,
     overviewLine,
     detailLine;
 
@@ -53,9 +54,7 @@ svg = d3.select("#visUN").append("svg").attr({
         transform: "translate(" + margin.left + "," + 0 + ")"
     });
 
-detailGraph = svg.append("g").attr({
-    transform: "translate(" + bbDetail.x + "," + bbDetail.y + ")"
-});
+
 
 overviewGraph = svg.append("g").attr({
     transform: "translate(" + bbOverview.x + "," + bbOverview.y + ")"
@@ -85,35 +84,34 @@ d3.csv("unHealth.csv", function(data) {
 
 
 function createVis(){
-    svg.data(dataSet);
-    console.log(dataSet);
     // console.log(dataWomensHealthMin, dataWomensHealthMax);
     yScaleOverview = d3.scale.linear()
         .domain([0, dataWomensHealthMax])
         .range([bbOverview.y + bbOverview.h + margin.top, 0 + margin.top]);
-
-    yScaleDetail = d3.scale.linear()
-        .domain([0, dataWomensHealthMax])
-        .range([bbDetail.y + bbDetail.h + margin.top, bbOverview.y + bbOverview.h + 30]);
-        // .range([ bbDetail.y, bbDetail.h + (-bbOverview.h) + (-bbDetail.y)]);
-
-    xScale = d3.time.scale()
+    xScaleOverview = d3.time.scale()
         // .domain([0, dataSetDates.length])
         .domain([dataSetDatesMin, dataSetDatesMax])
         .range([0, width]);
-
     overviewLine = d3.svg.line()
-        .x(function(d,i){var object; console.log(object = new Date(dataSet[i]["Analysis Date"])); return xScale(object)})
-        .y(function(d,i){return yScaleOverview(parseInt(dataSet[i]["Women's Health"]))})
-
-    detailLine = d3.svg.area()
-        .x(function(d,i){var object; console.log(object = new Date(dataSet[i]["Analysis Date"])); return xScale(object)})
-        .y1(function(d,i){return yScaleDetail(parseInt(dataSet[i]["Women's Health"]))})
-        .y0(bbDetail.y + bbDetail.h + margin.top)
-    // .y1(function(d) { return y(d.y); });
+        .x(function(d,i){var object; console.log(object = new Date(d["Analysis Date"])); return xScaleOverview(object)})
+        .y(function(d,i){return yScaleOverview(parseInt(d["Women's Health"]))});
 
 
 
+
+
+    brush = d3.svg.brush().x(xScaleOverview).on("brush", doBrush);
+
+
+
+    //Drawing Axes
+    xAxisOverview = d3.svg.axis()
+        .scale(xScaleOverview)
+        .orient("bottom")
+        .ticks(d3.time.months, 3)
+        // .tickFormat(d3.time.format('%a %d'))
+        .tickSize(0)
+        .tickPadding(8);
 
     yAxisOverview = d3.svg.axis()
         .scale(yScaleOverview)
@@ -122,75 +120,92 @@ function createVis(){
         .tickSize(0)
         .tickPadding(8);
 
-    yAxisDetail = d3.svg.axis()
-        .scale(yScaleDetail)
-        .orient("left")
-        .ticks(5)
-        .tickSize(0)
-        .tickPadding(8);
-
-    xAxis = d3.svg.axis()
-        .scale(xScale)
-        .orient("bottom")
-        .ticks(d3.time.months, 3)
-        // .tickFormat(d3.time.format('%a %d'))
-        .tickSize(0)
-        .tickPadding(8);
-
-
-    brush = d3.svg.brush().x(xScale).on("brush", brush);
-
-
-    //Drawing Axes
-    detailGraph.append("g")
-        .attr("class", "x axis")
-        .attr("id", "detailAxis")
-        .attr("transform", "translate(0, "+(bbDetail.y + bbDetail.h + margin.top)+")")
-        .call(xAxis);
-
     overviewGraph.append("g")
         .attr("class", "x axis")
         .attr("id", "overviewAxis")
         .attr("transform", "translate(0, "+(bbOverview.y + bbOverview.h + margin.top)+")")
-        .call(xAxis);
+        .attr("pointer-events", "none")
+        .call(xAxisOverview);
 
     overviewGraph.append("g")
         .attr("class", "y axis")
         .attr("id", "overviewAxisY")
         .call(yAxisOverview);
 
-    detailGraph.append("g")
-        .attr("class", "y axis")
-        .attr("id", "overviewAxisY")
-        .call(yAxisDetail);
-
-
     overviewGraph.append("svg:path")
-        .attr("d", overviewLine(dataSetWomensHealth))
+        .attr("d", overviewLine(dataSet))
         .attr("id", "overviewLine");
-
-    detailGraph.append("svg:path")
-        .attr("d", detailLine(dataSetWomensHealth))
-        .attr("id", "detailArea");        
-
 
     overviewGraph.selectAll(".dots")
         .data(dataSet)
         .enter()
     .append("circle")
         .attr("class", "dot")
-        .attr("cx", function(d,i){return xScale(new Date(d["Analysis Date"]))})
+        .attr("cx", function(d,i){return xScaleOverview(new Date(d["Analysis Date"]))})
         .attr("cy", function(d,i){return yScaleOverview(d["Women's Health"])})
         .attr("r", 2)
         .style("fill", "navy")
-        .style("stroke", "navy")
+        .style("stroke", "navy");
+
+        drawDetail();
+ }
+function drawDetail(){
+
+    if (detailGraph) {
+        detailGraph.remove();
+    }
+
+    detailGraph = svg.append("g").attr({
+        transform: "translate(" + bbDetail.x + "," + bbDetail.y + ")"
+    });
+    
+    xScaleDetail = d3.time.scale()
+        .domain([dataSetDatesMin, dataSetDatesMax])
+        .range([0, width]);
+    yScaleDetail = d3.scale.linear()
+        .domain([0, dataWomensHealthMax])
+        .range([bbDetail.y + bbDetail.h + margin.top, bbOverview.y + bbOverview.h + 30]);
+    detailLine = d3.svg.area()
+        .x(function(d,i){var object; console.log(object = new Date(d["Analysis Date"])); return xScaleDetail(object)})
+        .y1(function(d,i){return yScaleDetail(parseInt(d["Women's Health"]))})
+        .y0(bbDetail.y + bbDetail.h + margin.top);
+
+
+
+    xAxisDetail = d3.svg.axis()
+        .scale(xScaleDetail)
+        .orient("bottom")
+        .ticks(d3.time.months, 3)
+        // .tickFormat(d3.time.format('%a %d'))
+        .tickSize(0)
+        .tickPadding(8);
+
+    yAxisDetail = d3.svg.axis()
+        .scale(yScaleDetail)
+        .orient("left")
+        .ticks(5)
+        .tickSize(0)
+        .tickPadding(8);   
+
+    detailGraph.append("g")
+        .attr("class", "x axis")
+        .attr("id", "detailAxis")
+        .attr("transform", "translate(0, "+(bbDetail.y + bbDetail.h + margin.top)+")")
+        .call(xAxisDetail);
+    detailGraph.append("g")
+        .attr("class", "y axis")
+        .attr("id", "overviewAxisY")
+        .call(yAxisDetail);
+    detailGraph.append("svg:path")
+        .attr("d", detailLine(dataSet))
+        .attr("id", "detailArea");        
 
     detailGraph.selectAll(".dots")
         .data(dataSet)
         .enter()
     .append("circle")
         .attr("class", "dot")
-        .attr("cx", function(d,i){return xScale(new Date(d["Analysis Date"]))})
+        .attr("cx", function(d,i){return xScaleDetail(new Date(d["Analysis Date"]))})
         .attr("cy", function(d,i){return yScaleDetail(d["Women's Health"])})
         .attr("r", 2)
         .style("fill", "navy")
@@ -201,10 +216,12 @@ function createVis(){
             height: bbOverview.h + margin.top,
             transform: "translate(0, "+ (-margin.top) +")"
         })
-
-
- }
-
- function brush(){
+    detailGraph.select(".background").attr({
+            height: bbOverview.h + margin.top + 20,
+            transform: "translate(0, "+ (-margin.top) +")"
+        })
+}
+ function doBrush(e){
+    console.log(brush.extent());
 
  }
